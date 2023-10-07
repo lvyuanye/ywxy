3b92af0eb9174fb9094a4d700d2d5b5b04e224a3
lvyuanye <104409226+lvyuanye@users.noreply.github.com>
2023-10-07 12:30:48 +0800

    Update 1.sh

--- env/1.sh
+++ env/1.sh
@@ -5,7 +5,7 @@
 # Created Time: 2022年06月05日 星期日 10时46分19秒
 #########################################################################
 #!/bin/zsh
-PATH=/home/edison/bin:/home/edison/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
+PATH=/home/lyy/bin:/home/lyy/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
 export PATH
 #安装编译器
 (echo "Y") | sudo apt install build-essential
@@ -15,6 +15,7 @@ export PATH
 
 (echo "Y") | sudo apt-get install vim
 (echo "Y") | sudo apt-get install ctags
+(echo "Y") | sudo apt-get install exuberant-ctags
 
 #安装Vundle（vim插件）
 git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
@@ -24,7 +25,7 @@ vim +PluginInstall +qall;
 (echo "Y") | sudo apt install samba-common-bin
 sudo mkdir /var/lib/samba/usershares
 
-ln -s ~/ywxy/.vimrc ~/.vimrc
+ln -s ~/.vimrc ~/ywxy/.vimrc
 echo "source ~/ywxy/bashrc" >> ~/.bashrc
 
 # nerd 字体 内容较大，放到最后下载
